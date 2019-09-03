3501b41d25b10c8da836f5f9a920c988e6837014
JerryYangKai <kaiyan@microsoft.com>
2019-09-03 17:18:45 +0800

    add readme

---
+++ Device/main.ino
@@ -0,0 +1,372 @@
+//
+// Core header files for C and IoTHub layer
+//
+#include <stdio.h>
+#include "AZ3166WiFi.h"
+#include "IoT_DevKit_HW.h"
+#include "azure_c_shared_utility/connection_string_parser.h"
+#include "azure_c_shared_utility/string_tokenizer.h"
+#include "azure_c_shared_utility/threadapi.h"
+#include "azure_c_shared_utility/xlogging.h"
+#include "certs/certs.h"
+#include "src/mxchip_iot_devkit/pnp_device.h"
+#include "src/mxchip_iot_devkit/pnp_statistics.h"
+#include "src/mxchip_iot_devkit/ui/screen.h"
+#include "src/mxchip_iot_devkit/ui/setting_page.h"
+
+// IoT Central requires DPS.  Include required header and constants
+#include "azure_prov_client/iothub_security_factory.h"
+#include "azure_prov_client/prov_device_ll_client.h"
+#include "azure_prov_client/prov_transport_mqtt_client.h"
+#include "azure_prov_client/prov_security_factory.h"
+
+static bool wifiConnected = false;
+static bool iotHubConnected = false;
+
+// State of DPS registration process.  We cannot proceed with DPS until we get into the state APP_DPS_REGISTRATION_SUCCEEDED.
+typedef enum APP_DPS_REGISTRATION_STATUS_TAG
+{
+    APP_DPS_REGISTRATION_PENDING,
+    APP_DPS_REGISTRATION_SUCCEEDED,
+    APP_DPS_REGISTRATION_FAILED
+} APP_DPS_REGISTRATION_STATUS;
+
+const SECURE_DEVICE_TYPE secureDeviceTypeForProvisioning = SECURE_DEVICE_TYPE_SYMMETRIC_KEY;
+const IOTHUB_SECURITY_TYPE secureDeviceTypeForIotHub = IOTHUB_SECURITY_TYPE_SYMMETRIC_KEY;
+
+// DPSEndpoint=[DPS global endpoint];ScopeId=[Scope ID];RegistrationId=[Registration ID];SymmetricKey=[symmetric key]
+static const char *IOTHUBDPS_ENDPOINT = "DPSEndpoint";
+static const char *IOTHUBDPS_SCOPEID = "ScopeId";
+static const char *IOTHUBDPS_REGISTRATIONID = "RegistrationId";
+static const char *IOTHUBDPS_SYMMETRICKEY = "SymmetricKey";
+
+// The Device Provisioning Service (DPS) endpoint, learn more from https://docs.microsoft.com/en-us/azure/iot-dps/tutorial-set-up-device#create-the-device-registration-software.
+static char *globalDpsEndpoint = NULL;
+// The Device Provisioning Service (DPS) ID Scope.
+static char *dpsIdScope = NULL;
+// The symmetric key, learn more from https://docs.microsoft.com/en-us/azure/iot-dps/concepts-symmetric-key-attestation.
+static char *sasKey = NULL;
+// The Registration ID, learn more from https://docs.microsoft.com/en-us/azure/iot-dps/use-hsm-with-sdk.
+static char *registrationId = NULL;
+
+// TODO: Fill in DIGITALTWIN_DEVICE_CAPABILITY_MODEL_INLINE_DATA if want to make deivce self-describing.
+#define DIGITALTWIN_DEVICE_CAPABILITY_MODEL_INLINE_DATA "{}"
+
+static const char *digitalTwinSample_CustomProvisioningData = "{"
+                                                              "\"__iot:interfaces\":"
+                                                              "{"
+                                                              "\"CapabilityModelId\": \"urn:mxchip:mxchip_iot_devkit:1\" ,"
+                                                              "\"CapabilityModel\": \"" DIGITALTWIN_DEVICE_CAPABILITY_MODEL_INLINE_DATA "\""
+                                                              "}"
+                                                              "}";
+
+// Amount in ms to sleep between querying state from DPS registration loop
+static const int dpsRegistrationPollSleep = 100;
+
+// Maximum amount of times we'll poll for DPS registration being ready, 1 min.
+static const int dpsRegistrationMaxPolls = (60 * 1000 / dpsRegistrationPollSleep);
+
+// State of DigitalTwin registration process.  We cannot proceed with DigitalTwin until we get into the state APP_DIGITALTWIN_REGISTRATION_SUCCEEDED.
+typedef enum APP_DIGITALTWIN_REGISTRATION_STATUS_TAG
+{
+    APP_DIGITALTWIN_REGISTRATION_PENDING,
+    APP_DIGITALTWIN_REGISTRATION_SUCCEEDED,
+    APP_DIGITALTWIN_REGISTRATION_FAILED
+} APP_DIGITALTWIN_REGISTRATION_STATUS;
+
+#define IOT_HUB_CONN_STR_MAX_LEN 512
+
+static char *dpsIotHubUri;
+static char *dpsDeviceId;
+
+static bool parseDPSConnectionString(const char *connection_string)
+{
+    if (connection_string == NULL)
+    {
+        LogError("connection_string is NULL");
+        return false;
+    }
+    MAP_HANDLE connection_string_values_map;
+    if ((connection_string_values_map = connectionstringparser_parse_from_char(connection_string)) == NULL)
+    {
+        LogError("Tokenizing failed on connectionString");
+        return false;
+    }
+    const char *_globalDpsEndpoint = Map_GetValueFromKey(connection_string_values_map, IOTHUBDPS_ENDPOINT);
+    const char *_dpsIdScope = Map_GetValueFromKey(connection_string_values_map, IOTHUBDPS_SCOPEID);
+    const char *_sasKey = Map_GetValueFromKey(connection_string_values_map, IOTHUBDPS_SYMMETRICKEY);
+    const char *_registrationId = Map_GetValueFromKey(connection_string_values_map, IOTHUBDPS_REGISTRATIONID);
+    if (_globalDpsEndpoint)
+    {
+        mallocAndStrcpy_s(&globalDpsEndpoint, _globalDpsEndpoint);
+    }
+    else
+    {
+        LogError("Couldn't find %s in connection string", IOTHUBDPS_ENDPOINT);
+    }
+    if (_dpsIdScope)
+    {
+        mallocAndStrcpy_s(&dpsIdScope, _dpsIdScope);
+    }
+    else
+    {
+        LogError("Couldn't find %s in connection string", IOTHUBDPS_SCOPEID);
+    }
+    if (_sasKey)
+    {
+        mallocAndStrcpy_s(&sasKey, _sasKey);
+    }
+    else
+    {
+        LogError("Couldn't find %s in connection string", IOTHUBDPS_SYMMETRICKEY);
+    }
+    if (_registrationId)
+    {
+        mallocAndStrcpy_s(&registrationId, _registrationId);
+    }
+    else
+    {
+        LogError("Couldn't find %s in connection string", IOTHUBDPS_REGISTRATIONID);
+    }  
+    Map_Destroy(connection_string_values_map);
+
+    if (globalDpsEndpoint == NULL || dpsIdScope == NULL || sasKey == NULL || registrationId == NULL)
+    {
+        return false;
+    }
+    return true;
+}
+
+static void provisioningRegisterCallback(PROV_DEVICE_RESULT register_result, const char *iothub_uri, const char *device_id, void *user_context)
+{
+    APP_DPS_REGISTRATION_STATUS *appDpsRegistrationStatus = (APP_DPS_REGISTRATION_STATUS *)user_context;
+
+    if (register_result != PROV_DEVICE_RESULT_OK)
+    {
+        LogError("DPS Provisioning callback called with error state %d", register_result);
+        *appDpsRegistrationStatus = APP_DPS_REGISTRATION_FAILED;
+    }
+    else
+    {
+        if ((mallocAndStrcpy_s(&dpsIotHubUri, iothub_uri) != 0) ||
+            (mallocAndStrcpy_s(&dpsDeviceId, device_id) != 0))
+        {
+            LogError("Unable to copy provisioning information");
+            *appDpsRegistrationStatus = APP_DPS_REGISTRATION_FAILED;
+        }
+        else
+        {
+            LogInfo("Provisioning callback indicates success.  iothubUri=%s, deviceId=%s", dpsIotHubUri, dpsDeviceId);
+            *appDpsRegistrationStatus = APP_DPS_REGISTRATION_SUCCEEDED;
+        }
+    }
+}
+
+static bool registerDevice(bool traceOn)
+{
+    PROV_DEVICE_RESULT provDeviceResult;
+    PROV_DEVICE_LL_HANDLE provDeviceLLHandle = NULL;
+    bool result = false;
+
+    APP_DPS_REGISTRATION_STATUS appDpsRegistrationStatus = APP_DPS_REGISTRATION_PENDING;
+
+    if (IoTHub_Init() != 0)
+    {
+        LogError("IoTHub_Init failed");
+        return false;
+    }
+
+    if (prov_dev_set_symmetric_key_info(registrationId, sasKey) != 0)
+    {
+        LogError("prov_dev_set_symmetric_key_info failed.");
+    }
+    else if (prov_dev_security_init(secureDeviceTypeForProvisioning) != 0)
+    {
+        LogError("prov_dev_security_init failed");
+    }
+    else if ((provDeviceLLHandle = Prov_Device_LL_Create(globalDpsEndpoint, dpsIdScope, Prov_Device_MQTT_Protocol)) == NULL)
+    {
+        LogError("failed calling Prov_Device_Create");
+    }
+    else if ((provDeviceResult = Prov_Device_LL_SetOption(provDeviceLLHandle, PROV_OPTION_LOG_TRACE, &traceOn)) != PROV_DEVICE_RESULT_OK)
+    {
+        LogError("Setting provisioning tracing on failed, error=%d", provDeviceResult);
+    }
+    else if ((provDeviceResult = Prov_Device_LL_SetOption(provDeviceLLHandle, "TrustedCerts", certificates)) != PROV_DEVICE_RESULT_OK)
+    {
+        LogError("Setting provisioning TrustedCerts failed, error=%d", provDeviceResult);
+    }
+    else if ((provDeviceResult = Prov_Device_LL_Set_Provisioning_Payload(provDeviceLLHandle, digitalTwinSample_CustomProvisioningData)) != PROV_DEVICE_RESULT_OK)
+    {
+        LogError("Failed setting provisioning data, error=%d", provDeviceResult);
+    }
+    else if ((provDeviceResult = Prov_Device_LL_Register_Device(provDeviceLLHandle, provisioningRegisterCallback, &appDpsRegistrationStatus, NULL, NULL)) != PROV_DEVICE_RESULT_OK)
+    {
+        LogError("Prov_Device_Register_Device failed, error=%d", provDeviceResult);
+    }
+    else
+    {
+        // Pulling the registration status
+        for (int i = 0; (i < dpsRegistrationMaxPolls) && (appDpsRegistrationStatus == APP_DPS_REGISTRATION_PENDING); i++)
+        {
+            ThreadAPI_Sleep(dpsRegistrationPollSleep);
+            Prov_Device_LL_DoWork(provDeviceLLHandle);
+        }
+
+        if (appDpsRegistrationStatus == APP_DPS_REGISTRATION_SUCCEEDED)
+        {
+            LogInfo("DPS successfully registered.  Continuing on to creation of IoTHub device client handle.");
+            result = true;
+        }
+        else if (appDpsRegistrationStatus == APP_DPS_REGISTRATION_PENDING)
+        {
+            LogError("Timed out attempting to register DPS device");
+        }
+        else
+        {
+            LogError("Error registering device for DPS");
+        }
+    }
+
+    if (provDeviceLLHandle != NULL)
+    {
+        Prov_Device_LL_Destroy(provDeviceLLHandle);
+    }
+    IoTHub_Deinit();
+
+    return result;
+}
+
+static bool registerDeviceRetry(bool traceOn, int retryNumber, int retryInterval)
+{
+    char buff[32];
+    Screen.print(1, "Registering...\r\n  ");
+    for (int i = 0; i < retryNumber; i++)
+    {
+        if (registerDevice(traceOn))
+        {
+            return true;
+        }
+        delay(retryInterval);
+        snprintf(buff, sizeof(buff), " retry %d", i + 1);
+        Screen.print(2, buff);
+    }
+    return false;
+}
+
+static void connectIoTService(void)
+{
+    char buff[IOT_HUB_CONN_STR_MAX_LEN];
+    iotHubConnected = false;
+    digitalWrite(LED_AZURE, 0);
+
+    // Initialize device model application
+    if (registerDeviceRetry(false, 10, 10000))
+    {
+        Screen.print(1, "Connecting\r\n IoT Hub...");
+
+        buff[0] = 0;
+        if (secureDeviceTypeForProvisioning == SECURE_DEVICE_TYPE_SYMMETRIC_KEY)
+        {
+            snprintf(buff, sizeof(buff),
+                     "HostName=%s;DeviceId=%s;SharedAccessKey=%s",
+                     dpsIotHubUri,
+                     dpsDeviceId,
+                     sasKey);
+        }
+        else if (secureDeviceTypeForProvisioning == SECURE_DEVICE_TYPE_X509)
+        {
+            snprintf(buff, sizeof(buff),
+                     "HostName=%s;DeviceId=%s;UseProvisioning=true",
+                     dpsIotHubUri,
+                     dpsDeviceId);
+        }
+        
+        if (pnp_device_initialize(buff, certificates) != 0)
+        {
+            Screen.print(1, "Error: \r\nIoT Hub is not\r\navailable.");
+        }
+        else
+        {
+            digitalWrite(LED_AZURE, 1);
+            iotHubConnected = true;
+            screen_main();
+        }
+    }
+    else
+    {
+        Screen.print(1, "Error: \r\nRegistering\r\ndevice failed.\r\n");
+    }
+}
+
+void setup()
+{
+    char buff[64];
+
+    // Initialize the board
+    int ret = initIoTDevKit(1);
+    if (ret != 0)
+    {
+        if (ret == -100)
+        {
+            Screen.print(1, "No Wi-Fi.\r\n \r\n ");
+        }
+        else
+        {
+            Screen.print(1, "Internal error!\r\nCheck log for\r\n more detail.");
+        }
+        return;
+    }
+    else
+    {
+        IPAddress ip = WiFi.localIP();
+        snprintf(buff, sizeof(buff), "%s\r\nWiFi Connected\r\n%s", WiFi.SSID(), ip.get_address());
+        Screen.print(1, buff);
+        wifiConnected = true;
+    }
+
+    const char *connectionString = getIoTHubConnectionString();
+    if (!parseDPSConnectionString(connectionString))
+    {
+        Screen.print(0, "Setting error\r\nPlease re-config\r\nthe device\r\nin AP mode.");
+        wifiConnected = false;
+        return;
+    }
+
+    connectIoTService();
+}
+
+void loop()
+{
+    if (wifiConnected)
+    {
+        if (iotHubConnected)
+        {
+            pnp_device_run();
+
+            if (is_error_max())
+            {
+                setting_change(SETTING_CHANGE_NONE, 0);
+                telemetry_sent_reset();
+                error_reset();
+
+                pnp_device_close();
+                iotHubConnected = false;
+            }
+        }
+        else
+        {
+            // Retry
+            screen_main();
+            connectIoTService();
+        }
+    }
+
+    invokeDevKitPeripheral();
+    delay(500);
+}
+
+void __sys_setup(void)
+{
+    enable_sas_token_setting();
+}
--- README.md
+++ README.md
@@ -1,64 +1,164 @@
 ---
 page_type: sample
 languages:
-- csharp
+- javascript
+- nodejs
+- cpp
 products:
-- dotnet
-description: "Add 150 character max description"
-urlFragment: "update-this-to-unique-url-stub"
+- azure
+- azure-iot-hub
+name: "Azure MXChip IoT DevKit Using IoT Plug and Play"
+description: "This is the sample codes for MXChip IoT DevKit using IoT Plug and Play. Sample code to connect the DevKit to your Azure IoT Central application."
+urlFragment: "mxchip-iot-devkit-pnp"
 ---
 
-# Official Microsoft Sample
+## Sample: Connect the DevKit to your Azure IoT Central application
 
-<!-- 
-Guidelines on README format: https://review.docs.microsoft.com/help/onboard/admin/samples/concepts/readme-template?branch=master
+First, go to the [quickstart](https://docs.microsoft.com/azure/iot-central/howto-connect-devkit-pnp) tutorial to learn how to connect the DevKit to your Azure IoT Central application.
 
-Guidance on onboarding samples to docs.microsoft.com/samples: https://review.docs.microsoft.com/help/onboard/admin/samples/process/onboarding?branch=master
+### Review the code
 
-Taxonomies for products and languages: https://review.docs.microsoft.com/new-hope/information-architecture/metadata/taxonomies?branch=master
--->
+If you want to explore and modify the device code, follow these steps.
 
-Give a short description for your sample here. What does it do and why is it important?
+1. To download sample code for DevKit, run the following command on your desktop machine.
 
-## Contents
+   ```cmd/sh
+   git clone https://github.com/MXCHIP/IoTDevKit.git
+   ```
 
-Outline the file contents of the repository. It helps users navigate the codebase, build configuration and any related assets.
+    > If **git** is not installed in your development environment, you can download it from [https://git-scm.com/download](https://git-scm.com/download).
 
-| File/folder       | Description                                |
-|-------------------|--------------------------------------------|
-| `src`             | Sample source code.                        |
-| `.gitignore`      | Define what to ignore at commit time.      |
-| `CHANGELOG.md`    | List of changes to the sample.             |
-| `CONTRIBUTING.md` | Guidelines for contributing to the sample. |
-| `README.md`       | This README file.                          |
-| `LICENSE`         | The license for the sample.                |
+1. The source code is located in `pnp/iotc_devkit` folder.
 
-## Prerequisites
+To see how the telemetry is sent to the Azure IoT Central application, open the `sensor_interface.c` file.
 
-Outline the required components and tools that a user might need to have on their machine in order to run the sample. This can be anything from frameworks, SDKs, OS versions or IDE releases.
+- The function **SensorsInterface_Telemetry_SendAll** sends multiple telemetries including humidity, temperature, pressure, magnetometer, gyroscope and accelerometer .
+- The functions **Sensors_Serialize{XXXX}** in `digitaltwin_serializer.c` create the telemetry payload, using **Sensors_Telemetry_Read{XXXX}** functions in `mxchip_iot_devkit_impl.c` to get data from the sensors on the DevKit device.
 
-## Setup
+To see how property values are reported to the Azure IoT Central application, open the `deviceinfo_interface.c` file.
 
-Explain how to prepare the sample once the user clones or downloads the repository. The section should outline every step necessary to install dependencies and set up any settings (for example, API keys and output folders).
+- The function **DeviceinfoInterface_Property_ReportAll** sends properties including manufacturer, device model, software version, operating system name, processor architecture, processor manufacturer, total storage and total memory.
+- The functions in `digitaltwin_serializer.c` create the property payload.
 
-## Runnning the sample
+To see how the DevKit device responds to commands for LED actions called from the IoT Central application, open the `leds_interface.c` file.
 
-Outline step-by-step instructions to execute the sample and see its output. Include steps for executing the sample from the IDE, starting specific services in the Azure portal or anything related to the overall launch of the code.
+- The function **LedsInterface_Command_TurnOnLedCallback** processes the turnOnLed command. It uses **Leds_Command_TurnOnLed** function in `mxchip_iot_devkit_impl.c` to turn on the User LED.
+- The function **LedsInterface_Command_BlinkCallback** processes the blink command. It uses **Leds_Command_Blink** function in `mxchip_iot_devkit_impl.c` to blink the RGB LED.
 
-## Key concepts
+### Compile and upload code
 
-Provide users with more context on the tools and services used in the sample. Explain some of the code that is being used and how services interact with each other.
+If you plan to modify the code, you should follow these instructions to [prepare the development environment](https://docs.microsoft.com/azure/iot-hub/iot-hub-arduino-iot-devkit-az3166-get-started#prepare-the-development-environment) for your desktop operating system.
 
-## Contributing
+1. Configure Arduino extension to install latest DevKit. In VS Code, use **Ctrl+Shift+P** to open the command palette, enter and select **Preferences: Open User Settings**. Type **arduino** in the search bar and choose **Edit in settings.json**.
 
-This project welcomes contributions and suggestions.  Most contributions require you to agree to a
-Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
-the rights to use your contribution. For details, visit https://cla.opensource.microsoft.com.
+    ![Arduino setting](meida/howto-connect-devkit-pnp/arduino-setting.png)
 
-When you submit a pull request, a CLA bot will automatically determine whether you need to provide
-a CLA and decorate the PR appropriately (e.g., status check, comment). Simply follow the instructions
-provided by the bot. You will only need to do this once across all repos using our CLA.
+1. Add a line in `arduino.additionalUrls` section that points to the DevKit SDK that supports IoT Plug and Play.
 
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
-For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
-contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+    ```json
+    https://raw.githubusercontent.com/VSChina/azureiotdevkit_tools/pnp/package_mxchip_board_index.json
+    ```
+
+    ![Arduino additional URL](meida/howto-connect-devkit-pnp/add-url.png)
+
+1. Use **Ctrl+Shift+P** to open the command palette, enter and select **Arduino: Board Manager**.
+
+1. In the board manager page, choose **Refresh Package Indexes**.
+
+1. After the refresh completed, enter **AZ3166** in the search bar. Select and install latest version (>= 1.9.9).
+
+    ![Select DevKit SDK version](meida/howto-connect-devkit-pnp/select-version.png)
+
+    It takes sometimes to download and install the latest SDK.
+
+1. Check the MXCHIP AZ3166 is shown as selected board in the status bar and serial port with STMicroelectronics is used.
+
+   ![Select serial port](meida/howto-connect-devkit-pnp/select-serialport.png)
+
+   Your COM port may differ from the image. Click the serial port, choose the port with **STMicroelectronics** in command palette.
+
+   ![Select COM](meida/howto-connect-devkit-pnp/select-COM.png)
+
+   If the board type is incorrect, or there is no board type selected, click the board type and select **MXCHIP AZ3166** in the window that pops up.
+
+   ![Select board](meida/howto-connect-devkit-pnp/select-board.png)
+
+1. In `pnp/iotc_devkit` folder, open **iotc_devkit.code-workspace** in VS Code.
+
+1. Use **Ctrl+Shift+P** to open the command palette, enter and select **Arduino: Upload** to compile the project and upload to the DevKit.
+
+   In OUTPUT pane, you will see the compilation and upload progress.
+
+   ![Arduino upload done](meida/howto-connect-devkit-pnp/arduino-upload-done.png)
+
+1. Click **Open Serial Monitor** button status bar, then you will see the messages sent from your DevKit in OUTPUT pane.
+
+   ![Open serial monitor](meida/howto-connect-devkit-pnp/open-serial-monitor.png)
+
+   ![Serial monitor message](meida/howto-connect-devkit-pnp/serial-message.png)
+
+### Troubleshooting
+
+1. Download a serial client such as [Tera Term](https://tera-term.en.lo4d.com/windows).
+
+1. Connect the DevKit to your computer by USB.
+
+1. Open Tera Term, select **serial**, and then expand the port. The device should appear as an STMicroelectronics device. Choose **STMicroelectronics STLink Virtual COM Port**. Choose OK.
+
+    ![Select port](meida/howto-connect-devkit-pnp/select-port.png)
+
+1. Choose **Setup** on the menu bar, select **serial port**, and configure the connection speed to **115200** baud. Then choose OK to open the serial monitor.
+
+    ![Configure speed](meida/howto-connect-devkit-pnp/configure-speed.png)
+
+1. You can see the output log in the Window.
+
+    ![Serial message](meida/howto-connect-devkit-pnp/serial-message.png)
+
+### MXChip Device template details
+
+A device created from the MXChip IoT DevKit device template has the following characteristics:
+
+#### Telemetry
+
+Field name|Units|Minimum|Maximum|Decimal places
+-|-|-|-|-
+humidity|%|0|100|0
+temp|°C|-40|120|0
+pressure|hPa|260|1260|0
+magnetometerX|mgauss|-1000|1000|0
+magnetometerY|mgauss|-1000|1000|0
+magnetometerZ|mgauss|-1000|1000|0
+accelerometerX|mg|-2000|2000|0
+accelerometerY|mg|-2000|2000|0
+accelerometerZ|mg|-2000|2000|0
+gyroscopeX|mdps|-2000|2000|0
+gyroscopeY|mdps|-2000|2000|0
+gyroscopeZ|mdps|-2000|2000|0
+
+#### Properties
+
+Display name|Field name|Data type|Writable/Read only
+-|-|-|-
+Manufacturer|manufacturer|string|Read only
+Device model|model|string|Read only
+Software version|swVersion|string|Read only
+Operating system name|osName|string|Read only
+Processor architecture|processorArchitecture|string|Read only
+Processor manufacturer|processorManufacturer|string|Read only
+Total storage|totalStorage|long|Read only
+Total memory|totalMemory|long|Read only
+Fan Speed|fanSpeed|double|Writable
+Voltage|voltage|double|Writable
+Current|current|double|Writable
+IR|irSwitch|boolean|Writable
+
+#### Commands
+
+Field name|Input field name|Input field type
+-|-|-
+blink|interval|long
+turnOnLed|/|/
+turnOffLed|/|/
+echo|text|string
+countdown|number|integer
